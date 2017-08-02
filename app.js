4986f4434e8dd267a31795e39f8db406306b5977
Cameron Voell <cameronvoell@gmail.com>
2017-08-02 15:55:03 -0700

    Hide zcash form on mobile. Added info links to the bottom of the page

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 window.App = {
   start: function() {
-    
+     if (detectmob()) {
+       var inputDiv = document.getElementById('input');
+       inputDiv.style.display='none';
+     }
   },
   
   generateZcashCommand: function() {
+
      var returnZAddress = document.getElementById("returnZAddress").value;  
      var mailingAddress = document.getElementById("mailingAddress").value;  
      var radios = document.getElementsByName('tshirtSize');
@@ -48,4 +52,20 @@ function asciiToHexString(ascii) {
   
   return hex;
 
+}
+
+function detectmob() { 
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    return true;
+  }
+ else {
+    return false;
+  }
 }
